70cdc684a71df94971fef779b338712d51b73426
peter <15266320@qq.com>
2018-12-23 12:00:18 +0800

    引入bootstrap4.0,第一个TemplateView调用index页面

--- zw/urls.py
+++ zw/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic import TemplateView,RedirectView
+from zw.settings import *
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
+
 
 urlpatterns = [
+
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico')),
     path('admin/', admin.site.urls),
-]
+    path('',TemplateView.as_view(template_name="index.html"),name="index"),
+   ]
