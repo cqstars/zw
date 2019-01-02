5a52b6e91e3f4d737e14a795a9f96161f01d6ae8
peter <15266320@qq.com>
2019-01-02 17:17:42 +0800

    科目models设置完成，包括注册管理，上传阿里云

--- apps/AccountingSubjects/admin.py
+++ apps/AccountingSubjects/admin.py
@@ -8,6 +8,12 @@ class AccountingSubjectCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(AccountingSubject)
 class AccountingSubjectAdmin(admin.ModelAdmin):
-    list_display = ('AccountingSubjectNo','name','scopeofapplication','category')
+    list_display = ('AccountingSubjectNo','name','scopeofapplication','dr_amount','cr_amount','category')
+    list_per_page = 50
+    ordering = ('id',)
+
+@admin.register(AccountingSubject_2)
+class AccountingSubject_2Admin(admin.ModelAdmin):
+    list_display = ('subject2_no', 'name', 'scopeofapplication', 'dr_amount', 'cr_amount', 'subject')
     list_per_page = 50
     ordering = ('id',)
--- apps/AccountingSubjects/models.py
+++ apps/AccountingSubjects/models.py
@@ -14,10 +14,27 @@ class AccountingSubject(models.Model):
     category=models.ForeignKey(AccountingSubjectCategory,on_delete=models.CASCADE,verbose_name="会计科目大类")
     AccountingSubjectNo=models.CharField(max_length=5,verbose_name="编号")
     name=models.CharField(max_length=15,verbose_name="会计科目名称")
+    dr_amount=models.DecimalField(max_digits=9,decimal_places=2,default=0,verbose_name="借方金额")
+    cr_amount=models.DecimalField(max_digits=9,decimal_places=2,default=0,verbose_name="贷方金额")
     scopeofapplication=models.CharField(max_length=15,verbose_name="会计科目适用范围")
 
     class Meta:
         verbose_name = "会计科目"
+        verbose_name_plural=verbose_name
+    def __str__(self):
+        return self.name
+
+
+class AccountingSubject_2(models.Model):
+    subject=models.ForeignKey(AccountingSubject,on_delete=models.CASCADE,verbose_name="会计科目")
+    subject2_no=models.CharField(max_length=5,verbose_name="编号")
+    name=models.CharField(max_length=15,verbose_name="会计科目二级名称")
+    dr_amount=models.DecimalField(max_digits=9,decimal_places=2,default=0,verbose_name="借方金额")
+    cr_amount=models.DecimalField(max_digits=9,decimal_places=2,default=0,verbose_name="贷方金额")
+    scopeofapplication=models.CharField(max_length=15,verbose_name="会计科目适用范围")
+
+    class Meta:
+        verbose_name = "会计科目二级"
         verbose_name_plural=verbose_name
     def __str__(self):
         return self.name
